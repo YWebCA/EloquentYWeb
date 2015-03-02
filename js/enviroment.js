f7977d0bc6fbdf3cd2eebf03ce8552cdc1e80ed5
Nathan Stier <nathan.stier@gmail.com>
2015-03-02 03:58:03 -0600

    Add custommatcher for JSHint
    Signed-off-by: Nathan Stier <nathan.stier@gmail.com>

--- js/enviroment.js
+++ js/enviroment.js
@@ -1,1 +1,18 @@
 // World enviroment file
+  var customMatchers = {};
+
+// JSHint Matcher
+customMatchers.toPassJSHint =  function(util, customEqualityTesters) {
+  return {
+    compare: function(actual, expected) {
+      var options = {curly: true, white: true, indent: 2, maxdepth: 3};
+      var pass = JSHINT(actual, options);
+      var message = "";
+      console.log(JSHINT.errors);
+      for (error of JSHINT.errors) {
+        message += "line " + error.line + " character " + error.character + ' - ' + error.reason + ' - ' + error.evidence + "\n";
+      }
+      return { pass: pass, message: message };
+    }
+  }
+}
