828f79bc41b015af723348a7029b4619bfc5701d
Nathan Stier <nathan.stier@gmail.com>
2015-03-02 04:20:26 -0600

    Add options to JSHint
    Signed-off-by: Nathan Stier <nathan.stier@gmail.com>

--- js/enviroment.js
+++ js/enviroment.js
@@ -5,7 +5,16 @@
 customMatchers.toPassJSHint =  function(util, customEqualityTesters) {
   return {
     compare: function(actual, expected) {
-      var options = {curly: true, white: true, indent: 2, maxdepth: 3};
+      var options = {
+        curly: true,
+        white: true,
+        indent: 2,
+        maxdepth: 3,
+        bitwise: true,
+        eqeqeq: true,
+        nonew: true,
+        newcap: true,
+        };
       var pass = JSHINT(actual, options);
       var message = "";
       console.log(JSHINT.errors);
