f4958b228111cf4823f3c6ccc54ed9d7be873d38
Annalise "Lulu" Haynes <lulu@lonelyrobotstudio.com>
2015-03-03 15:26:23 -0600

    Numbers exercises

--- chapter01/js/exercises.js
+++ chapter01/js/exercises.js
@@ -3,7 +3,32 @@
 /*-----------------------------    NUMBERS    ------------------------------*/
 
 // Numbers 1
+// Make the function return a value that is a number.
 Exercises.numbers1 = function() {
+
+  return /* your answer here */ ;
+
+};
+
+// Numbers 2
+// Make the function return a value that is a negative number.
+Exercises.numbers2 = function() {
+
+  return /* your answer here */ ;
+
+};
+
+// Numbers 3
+// Make the function return a value that is between 4 and 5.
+Exercises.numbers3 = function() {
+
+  return /* your answer here */ ;
+
+};
+
+// Numbers 4
+// Make the function return a value that is between 10e4 and 10e5.
+Exercises.numbers4 = function() {
 
   return /* your answer here */ ;
 
--- chapter01/js/spec.js
+++ chapter01/js/spec.js
@@ -11,10 +11,32 @@ describe("Exercise file", function() {
   });
 });
 
-describe("Numbers exercises", function () {
-  describe("Numbers 1", function () {
+describe("Numbers exercises", function() {
+
+  describe("Numbers 1", function() {
     it("should return a value that is a number", function() {
-      expect(typeof Exercises.numbers1()).toBe('number');
+      expect( typeof Exercises.numbers1() ).toBe('number');
     });
   });
+
+  describe("Numbers 2", function() {
+    it("should return a value that is a negative number", function() {
+      expect( Exercises.numbers2() ).toBeLessThan(0);
+    });
+  });
+
+  describe("Numbers 3", function() {
+    it("should return a value that is between 4 and 5", function() {
+      expect( Exercises.numbers3() ).toBeGreaterThan(4);
+      expect( Exercises.numbers3() ).toBeLessThan(5);
+    });
+  });
+
+  describe("Numbers 4", function() {
+    it("should return a value that is between 10e4 and 10e5", function() {
+      expect( Exercises.numbers3() ).toBeGreaterThan(10e4);
+      expect( Exercises.numbers3() ).toBeLessThan(10e5);
+    });
+  });
+
 });
