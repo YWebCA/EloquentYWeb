58e83b35dc93ff1fa8065d733b40d31a9b6dab83
Nathan Stier <nathan.stier@gmail.com>
2015-03-04 15:29:55 -0600

    Add getFile to JSHint spec
    Signed-off-by: Nathan Stier <nathan.stier@gmail.com>

--- chapter11/js/spec.js
+++ chapter11/js/spec.js
@@ -2,11 +2,9 @@
 describe("Exercise file", function() {
   it("should pass JSHint", function(done) {
     jasmine.addMatchers(customMatchers);
-    $.ajax("js/exercises.js", {
-      success: function(data) {
-        expect(data).toPassJSHint();
-        done();
-      }
-    })
+    getFile("js/exercises.js", function(err, data) {
+      expect(data).toPassJSHint();
+      done();
+    });
   });
 });
