c92c0d0d5c52c21d74ef1490be749d2d70a3d843
Nathan Stier <nathan.stier@gmail.com>
2015-03-09 20:57:25 -0500

    remove old jshint
    Signed-off-by: Nathan Stier <nathan.stier@gmail.com>

--- chapter03/js/spec.js
+++ chapter03/js/spec.js
@@ -1,10 +1,1 @@
 // Exercise tests
-describe("Exercise file", function() {
-  it("should pass JSHint", function(done) {
-    jasmine.addMatchers(customMatchers);
-    getFile("js/exercises.js", function(err, data) {
-      expect(data).toPassJSHint();
-      done();
-    });
-  });
-});
