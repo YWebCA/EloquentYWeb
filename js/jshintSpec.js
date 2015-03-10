9d51d41dec0e78baabbb4e4c9c9d8f8735eeb45b
Nathan Stier <nathan.stier@gmail.com>
2015-03-09 19:25:03 -0500

    Move JSHint spec to seperate file
    Signed-off-by: Nathan Stier <nathan.stier@gmail.com>

---
+++ js/jshintSpec.js
@@ -0,0 +1,9 @@
+describe("Exercise file", function() {
+  it("should pass JSHint", function(done) {
+    jasmine.addMatchers(customMatchers);
+    getFile("js/exercises.js", function(err, data) {
+      expect(data).toPassJSHint();
+      done();
+    });
+  });
+});
