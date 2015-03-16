abfb863a6673024277e3b15feca75bfbc4f4a174
Annalise "Lulu" Haynes <lulu@lonelyrobotstudio.com>
2015-03-16 13:58:27 -0500

    Expressions and statements

--- chapter02/js/exercises.js
+++ chapter02/js/exercises.js
@@ -1,1 +1,18 @@
-// Student Workspace
+/*---------------------    EXPRESSIONS AND STATEMENTS    ---------------------*/
+
+// Expressions 1
+// Make the function return a nested expression (use parentheses) whose
+// evaluated value is not undefined.
+Exer.expressions1 = function() {
+
+  return /* your answer here */ ;
+
+};
+
+// Statements 1
+// Write a statement that is not undefined.
+Exer.statements1 = function() {
+
+  /* your answer here */
+
+};
--- chapter02/js/spec.js
+++ chapter02/js/spec.js
@@ -1,163 +1,195 @@
 // Annalise
 describe("Expressions and Statements exercises", function() {
-	describe("", function() {
-		it("should ", function() {
-			
-		});
-	});
+
+  describe("Expressions 1", function() {
+
+    it("should return an expression that is not undefined", function() {
+      expect( Exer.expressions1() ).not.toBe(undefined);
+    });
+
+    it("should return an expression with parentheses in it", function() {
+      expect( Exer.expressions1.toString().match(/^\s*return.*;/mg)[0].match(/(?:\(|\)).+(?:\(|\))/g) ).not.toBeNull();
+    });
+
+  });
+
+  describe("Statements 1", function() {
+
+    beforeAll(function() {
+      this.work = Exer.statements1.toString().match(/^.*$/mg);  // break into lines
+      for ( var i = 0; i < this.work.length; i++ ) {
+        if ( !this.work[i] ) {
+          this.work.splice(i, 1);  // remove empty lines
+        }
+      }
+      this.work = this.work.slice(1, length - 1);  // remove function expression wrapper
+    });
+
+    it("should contain a single statement", function() {
+      expect( this.work ).not.toBe(null);           // there should be lines left
+      expect( this.work.length ).toBe(1);           // in fact, one line
+      this.work = this.work[0].match(/\s*(.*);/g);  // find statements
+      expect( this.work ).not.toBe(null);
+    });
+
+    it("should evaluate to something other than undefined", function() {
+      expect( eval(this.work.match(/\s*(.*);/)[1]) ).not.toBeUndefined();
+    });
+
+  });
 });
 
 // Nate
 describe("Variables exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Zach
 describe("Keywords and Reserved Words exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Annalise
 describe("The Environment exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Nate
 describe("Functions exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Zach
 describe("The console.log Function exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Annalise
 describe("Return Values exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Nate
 describe("Prompt and Confirm exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Zach
 describe("Control Flow exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Annalise
 describe("Conditional Execution exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Nate
 describe("while and dowhile Loops exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Zach
 describe("Indenting Code exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Annalise
 describe("for Loops exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Nate
 describe("Breaking Out of a Loop exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Zach
 describe("Updating Variables Succinctly exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Annalie
 describe("Dispatching on a Value with switch exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Nate
 describe("Capitalization exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 // Zach
 describe("Comments exercises", function() {
-	describe("", function() {
-		it("should ", function() {
+  describe("", function() {
+    it("should ", function() {
 
-		});
-	});
+    });
+  });
 });
 
 describe("Exercise file", function() {
