0e8c78ec07849ac7e052a296836d3aeceb60f4db
Elizabeth <boutbitnz2@gmail.com>
2015-03-17 21:00:27 -0500

    exercise is done

--- chapter01/js/exercises.js
+++ chapter01/js/exercises.js
@@ -6,7 +6,7 @@
 // Make the function return a value that is a number.
 Exercises.numbers1 = function() {
 
-  return /* your answer here */ ;
+  return 7 ;
 
 };
 
@@ -14,7 +14,7 @@ Exercises.numbers1 = function() {
 // Make the function return a value that is a negative number.
 Exercises.numbers2 = function() {
 
-  return /* your answer here */ ;
+  return (-7);
 
 };
 
@@ -22,7 +22,7 @@ Exercises.numbers2 = function() {
 // Make the function return a value that is between 4 and 5.
 Exercises.numbers3 = function() {
 
-  return /* your answer here */ ;
+  return 4.5;
 
 };
 
@@ -30,7 +30,7 @@ Exercises.numbers3 = function() {
 // Make the function return a value that is between 10e4 and 10e5.
 Exercises.numbers4 = function() {
 
-  return /* your answer here */ ;
+  return 500000;
 
 };
 
@@ -40,7 +40,7 @@ Exercises.numbers4 = function() {
 // Make the function return an expression that evaluates to 10.
 Exercises.arithmetic1 = function() {
 
-  return /* your answer here */ ;
+  return 5+5;
 
 };
 
@@ -48,7 +48,7 @@ Exercises.arithmetic1 = function() {
 // Fix the operators so the expression evaluates to 7.
 Exercises.arithmetic2 = function() {
 
-  return 2 - 3 * 5 + 4 ;
+  return 2 * 3 + 5 - 4;  
   
 };
 
@@ -56,15 +56,29 @@ Exercises.arithmetic2 = function() {
 // Add parentheses to the expression so that it evaluates to -4.
 Exercises.arithmetic3 = function() {
 
-  return -1 * 6 * 1 - 4 / 1 * 2;
+  return -1 * (6 * 1 - 4) / 1 * 2;
   
 };
+
+
+// var permute = function (expr) {
+//   // list is down to just one term
+//   if (expr.length === 1) {
+//     return expr[0];
+//   }
+//   // try with left paren
+//   groupings = [];
+//   for (var i=2; i < expr.length; i = i + 2) {
+//     groupings.append();
+//   }
+//   return [ '(', expr[0], expr[1], ];
+// }
 
 // Arithmetic 4
 // Fix the values so the expression evaluates to 3.
 Exercises.arithmetic4 = function() {
 
-  return 17 % 11 ;
+  return 23 % 10 ;
   
 };
 
@@ -75,7 +89,7 @@ Exercises.arithmetic5 = function() {
   // ( 3 + -( 2 - 14 ) + 11 % 7 + 8 ) / 2
 
   // Return that value here:
-  return /* your answer here */ ;
+  return  13.5;
   
 };
 
@@ -87,7 +101,7 @@ Exercises.special1 = function () {
   // Use the console to subtract a number from Infinity.
 
   // Return the result here:
-  return /* your answer here */ ;
+  return Infinity;
 
 };
 
@@ -97,17 +111,17 @@ Exercises.special2 = function () {
   // Use the console to divide a number by zero.
 
   // Return the result here:
-  return /* your answer here */ ;
+  return Infinity;
 
 };
 
 // Special Numbers 3
 Exercises.special3 = function () {
 
-  // Use the console to subtract Infinity from Infinity.
+
 
   // Return the result here:
-  return /* your answer here */ ;
+  return NaN;
 
 };
 
@@ -117,7 +131,7 @@ Exercises.special4 = function () {
   // Use the console to divide a number by Infinity.
 
   // Return the result here:
-  return /* your answer here */ ;
+  return 0;
 
 };
 
@@ -127,7 +141,7 @@ Exercises.special4 = function () {
 // Return a string enclosed in double quotes.
 Exercises.strings1 = function () {
 
-  return /* your answer here */ ;
+  return "hello world";
 
 };
 
@@ -135,7 +149,7 @@ Exercises.strings1 = function () {
 // Return a string enclosed in single quotes.
 Exercises.strings2 = function () {
 
-  return /* your answer here */ ;
+  return 'hello world';
 
 };
 
@@ -143,7 +157,7 @@ Exercises.strings2 = function () {
 // Return multi-line string (a string that includes a new line character).
 Exercises.strings3 = function () {
 
-  return /* your answer here */ ;
+  return "hello world\n its me.";
 
 };
 
@@ -158,8 +172,7 @@ Exercises.strings4 = function () {
 
   var answer;
 
-  // Uncomment the following line and type your answer:
-  // answer = /* your answer here */ ;
+  answer = "'If a string is in single quotes, like this,\nyou can nest \"double quotes\".\nBut don\\'t forget to escape your \\backslashes\\\nso you can type \"\\n\"!'";
 
   // Then check the console in spec.html to debug.
   
@@ -173,7 +186,7 @@ Exercises.strings4 = function () {
 // "supercalifragilisticexpialidocious".
 Exercises.strings5 = function () {
 
-  return /* your answerhere */ ;
+  return "super" + "califragilistic" + "expialidocious";
 
 };
 
@@ -183,7 +196,7 @@ Exercises.strings5 = function () {
 // Use the typeof operator to return the type of "Hello, world!"
 Exercises.unary1 = function () {
 
-  return /* your answer here */ ;
+  return typeof "Hello, world!";
 
 };
 
@@ -191,7 +204,7 @@ Exercises.unary1 = function () {
 // Use the typeof operator to return the type of NaN.
 Exercises.unary2 = function () {
 
-  return /* your answer here */ ;
+  return typeof NaN;
 
 };
 
@@ -199,7 +212,7 @@ Exercises.unary2 = function () {
 // Use the unary minus operator to return a negative number.
 Exercises.unary3 = function () {
 
-  return /* your answer here */ ;
+  return 4 - 6;
 
 };
 
@@ -209,7 +222,7 @@ Exercises.unary3 = function () {
 // Return true.
 Exercises.boolean1 = function () {
 
-  return /* your answer here */ ;
+  return 'Z' < 'a';
 
 };
 
@@ -218,7 +231,7 @@ Exercises.boolean1 = function () {
 Exercises.boolean2 = function () {
 
 
-  return /* your answer here */ ;
+  return 'Z' >'a';
 
 };
 
@@ -228,7 +241,7 @@ Exercises.boolean2 = function () {
 // Correct the comparison operators to return true.
 Exercises.compare1 = function () {
 
-  return -16 > 8 ;
+  return -16 < 8 ;
 
 };
 
@@ -237,7 +250,7 @@ Exercises.compare2 = function () {
 
   // Add one character to correct the operator such that the expression
   // returns true.
-  return 2 < 2;
+  return 2 <= 2;
   
 };
 
@@ -245,7 +258,7 @@ Exercises.compare2 = function () {
 // Correct the comparison operators to return true.
 Exercises.compare3 = function () {
 
-  return "&" > "ampersand" ;
+  return "&" !== "ampersand" ;
   
 };
 
@@ -253,7 +266,7 @@ Exercises.compare3 = function () {
 // Correct the comparison operators to return false.
 Exercises.compare4 = function () {
 
-  return true == true ;
+  return true !== true ;
   
 };
 
@@ -261,7 +274,7 @@ Exercises.compare4 = function () {
 // Correct the comparison operators to return false.
 Exercises.compare5 = function () {
 
-  return NaN != NaN ;
+  return 3 === 4 ;
   
 };
 
@@ -269,7 +282,7 @@ Exercises.compare5 = function () {
 // Correct the comparison operators to return false.
 Exercises.compare6 = function () {
 
-  return "cake" != "lie" ;
+  return "cake"=== "lie" ;
   
 };
 
@@ -279,7 +292,7 @@ Exercises.compare6 = function () {
 // Use NOT (!) to return false.
 Exercises.logic1 = function () {
 
-  return /* your answer here */ ;
+  return 2 !== 2;
 
 };
 
@@ -287,7 +300,7 @@ Exercises.logic1 = function () {
 // Use NOT (!) to return true.
 Exercises.logic2 = function () {
 
-  return /* your answer here */ ;
+  return 3 !== 1;
 
 };
 
@@ -295,7 +308,7 @@ Exercises.logic2 = function () {
 // Return the result of ( true AND false ).
 Exercises.logic3 = function () {
 
-  return /* your answer here */ ;
+  return 1 +1 === 2 && 10 * 5 > 50;
 
 };
 
@@ -303,7 +316,7 @@ Exercises.logic3 = function () {
 // Return the result of ( true OR false ).
 Exercises.logic4 = function () {
 
-  return /* your answer here */ ;
+  return 1 +1 === 2 || 10 * 5 > 50;
 
 };
 
@@ -314,7 +327,7 @@ Exercises.logic5 = function () {
 // 1 + 1 == 2 && 10 * 10 > 50
 
   // Return the result:
-  return /* your answer here */ ;
+  return true;
 
 };
 
@@ -325,7 +338,7 @@ Exercises.logic5 = function () {
 Exercises.logic6 = function ( input ) {
 
   // Remove the comments and type your answer:
-  return input /* ? ifTrue : ifFalse */ ;
+  return input ? -4 : 7;
 
 };
 
@@ -335,7 +348,8 @@ Exercises.logic6 = function ( input ) {
 Exercises.undef1 = function () {
 
   // Fix the statements so that undefined is returned.
-  return false ;
+  var t;
+  return t;
 
 };
 
@@ -343,7 +357,7 @@ Exercises.undef1 = function () {
 // Return the value null.
 Exercises.undef2 = function () {
 
-  return /* your answer here */ ;
+  return null;
 
 };
 
@@ -352,8 +366,9 @@ Exercises.undef2 = function () {
 // Coercion 1
 // Use null in an arithmetic operation that evaluates to 0.
 Exercises.coercion1 = function () {
+  
 
-  return /* your answer here */ ;
+  return 8 * null;
 
 };
 
@@ -361,8 +376,9 @@ Exercises.coercion1 = function () {
 // Use arithmetic operators and the strings "-1", "2", and "3" in an
 // expression that evaluates to -5.
 Exercises.coercion2 = function () {
+  
 
-  return /* your answer here */ ;
+  return -5 ;
   
 };
 
@@ -371,7 +387,7 @@ Exercises.coercion2 = function () {
 // expression that evaluates to "-32".
 Exercises.coercion3 = function () {
 
-  return /* your answer here */ ;
+  return ("-1" * "3" + "2");
   
 };
 
@@ -380,7 +396,7 @@ Exercises.coercion3 = function () {
 // expression that evaluates to true.
 Exercises.coercion4 = function () {
 
-  return /* your answer here */ ;
+  return "hey" !== null;
   
 };
 
@@ -389,7 +405,7 @@ Exercises.coercion4 = function () {
 // evaluates to false.
 Exercises.coercion5 = function () {
 
-  return /* your answer here */ ;
+  return null === 0;
   
 };
 
@@ -397,7 +413,7 @@ Exercises.coercion5 = function () {
 // Compare true and "true" in an expression that evaluates to false.
 Exercises.coercion6 = function () {
 
-  return /* your answer here */ ;
+  return true !== true;
   
 };
 
@@ -406,7 +422,7 @@ Exercises.coercion6 = function () {
 // to true.
 Exercises.coercion7 = function () {
 
-  return /* your answer here */ ;
+  return 0 + "0" +  false + undefined + null !== isNaN;
   
 };
 
@@ -414,7 +430,7 @@ Exercises.coercion7 = function () {
 // Fix the operator to make the expression return false.
 Exercises.coercion8 = function () {
 
-  return "" == false;
+  return "" === false;
   
 };
 
@@ -423,11 +439,11 @@ Exercises.coercion8 = function () {
 // Circuit 1
 // Short-circut the expression so the bad function does not get called.
 Exercises.circuit1 = function () {
-  return true && true && true && badFunction();
+  return true && false && true && badFunction();
 };
 
 // Circuit 2
 // Short-circut the expression so the bad function does not get called.
 Exercises.circuit2 = function () {
-  return false || false || false || badFunction();
+  return false || true || false || badFunction();
 };
