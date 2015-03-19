a6ef68c99bff1b91e2fdc1e9a6d8fc1cf665c2b8
KoeSeph <koesavant@gmail.com>
2015-03-18 19:11:52 -0500

    fizzybish

--- chapter02/js/fizzbuzz.js
+++ chapter02/js/fizzbuzz.js
@@ -1,18 +1,19 @@
 // Student Workspace
 Fizz.buzz = function () {
-  var answer = [];
-  for ( var i = 1; i <= 100; i++ ) {
-    var output = 0;
-    if ( i % 3 == 0 ) {
-      output += "Fizz";
-    } else {
-      output += i;
-    }
-    if ( i % 5 == 0 ) {
-      output += "Buzz";
-    }
-    
-    answer.push( [ output ] );
-    console.log(output)
+ var answer = [];
+ for ( var i = 1; i <= 100; i++ ) {
+  var output = [];
+  if ( i % 15 == 0 ) {
+     output += "FizzBuzz";
+  } else if ( i % 5 == 0 ) {
+     output += "Buzz";
+  } else if ( i % 3 == 0) {
+     output += "Fizz"
+  }else {
+     output += i;
   }
+  answer.push( output );
+  console.log(Number(output) || output);
+};
+
 };
