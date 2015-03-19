dd8f3663a7e25fbbc6e80c050fa436196c533664
KoeSeph <koesavant@gmail.com>
2015-03-18 19:19:03 -0500

    chessboard done

--- chapter02/js/chessboard.js
+++ chapter02/js/chessboard.js
@@ -1,35 +1,19 @@
 // Student Workspace
-Chess.start = function (input) {
-var output = '';
-for ( var i = 1; i <= (input * input); i++) {
- if ( isEven(i)) {
-   output += " ";
- }
- if ( i < 2 ){
-   if ( i % input ){
-     output += "#";
-   }
-            
- }else if( i % input === 0 ) {
-   output += "\n";
- }
-        
- if ( i % (input * input) === 0 ) {
-   output += "#";
- }
- if ( isOdd(i) ) {
-   output += "#";
- }
-}
-
-function isEven(n) {
- return isNumber(n) && (n % 2 === 0);
-}
-
-function isOdd(n){
-  return isNumber(n) && (Math.abs(n) % 2 === 1);
-}
-function isNumber(n){
- return n == parseFloat(n);
-}
+Chess.start = function (n) {
+  var size;
+  if ( n !== 0 ) {
+    size = n || 8;
+  } else {size = 0;}
+  var output = "";
+  for ( var i = 0; i < size; i++ ) {
+    for ( var j = 0; j < size; j++ ) {
+      if ( (i+j) % 2 == 0 ) {
+        output += ' ';
+      } else {
+        output += '#';
+      }
+    }
+    output += '\n';
+  }
+  return output;
 };
