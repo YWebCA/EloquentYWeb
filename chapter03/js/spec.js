0331d4b4abb21bf60dda9cd8a7c4736b2b05f2b1
Nathan Stier <nathan.stier@gmail.com>
2015-04-02 17:21:38 -0500

    Stub spec
    Signed-off-by: Nathan Stier <nathan.stier@gmail.com>

--- chapter03/js/spec.js
+++ chapter03/js/spec.js
@@ -1,1 +1,51 @@
 // Exercise tests
+describe("Defining a Function exercises", function() {
+  describe("Defining 1", function() {
+    it("should define a function named three that returns 3");
+  describe("Defining 2", function() {
+    it("should define a function named myLog accepts a value and then logs it");
+  });
+  describe("Defining 3", function() {
+    it("should define a function named square accepts a number and squares it");
+  });
+});
+
+describe("Parameters and Scopes exercises", function() {
+  it("should test a thing");
+});
+
+describe("Nested Scope exercises", function() {
+  it("should test a thing");
+});
+
+describe("Functions as Values exercises", function() {
+  it("should test a thing");
+});
+
+describe("DeclarationNotation exercises", function() {
+  it("should test a thing");
+});
+
+describe("The Call Stack exercises", function() {
+  it("should test a thing");
+});
+
+describe("Optional Arguments exercises", function() {
+  it("should test a thing");
+});
+
+describe("Closure exercises", function() {
+  it("should test a thing");
+});
+
+describe("Recursion exercises", function() {
+  it("should test a thing");
+});
+
+describe("Growing Functions exercises", function() {
+  it("should test a thing");
+});
+
+describe("Functions and Side Effects exercises", function() {
+  it("should test a thing");
+});
