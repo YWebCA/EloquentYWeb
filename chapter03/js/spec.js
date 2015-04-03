5ff1b6ef7919b5cb327de82014463331490065f8
Annalise "Lulu" Haynes <lulu@lonelyrobotstudio.com>
2015-04-03 14:46:08 -0500

    Assign tasks

--- chapter03/js/spec.js
+++ chapter03/js/spec.js
@@ -1,4 +1,5 @@
-// Exercise tests
+/*************************    Defining a Function    **************************/
+// Nate
 describe("Defining a Function exercises", function() {
   describe("Defining 1", function() {
     it("should define a function named three that returns 3");
@@ -10,42 +11,62 @@ describe("Defining a Function exercises", function() {
   });
 });
 
+/************************    Parameters and Scopes    *************************/
+// Nate
 describe("Parameters and Scopes exercises", function() {
   it("should test a thing");
 });
 
+/*****************************    Nested Scope    *****************************/
+// Annalise
 describe("Nested Scope exercises", function() {
   it("should test a thing");
 });
 
+/*************************    Functions as Values    **************************/
+// Nate
 describe("Functions as Values exercises", function() {
   it("should test a thing");
 });
 
+/*************************    Declaration Notation    *************************/
+// Annalise
 describe("DeclarationNotation exercises", function() {
   it("should test a thing");
 });
 
+/****************************    The Call Stack    ****************************/
+// Nate
 describe("The Call Stack exercises", function() {
   it("should test a thing");
 });
 
+/**************************    Optional Arguments    **************************/
+// Annalise
 describe("Optional Arguments exercises", function() {
   it("should test a thing");
 });
 
+/*******************************    Closure    ********************************/
+// Nate
 describe("Closure exercises", function() {
   it("should test a thing");
 });
 
+/******************************    Recursion    *******************************/
+// Annalise
 describe("Recursion exercises", function() {
   it("should test a thing");
 });
 
+/**************************    Growing Functions    ***************************/
+// Nate
 describe("Growing Functions exercises", function() {
   it("should test a thing");
 });
 
+/**********************    Functions and Side Effects    **********************/
+// Annalise
 describe("Functions and Side Effects exercises", function() {
   it("should test a thing");
 });
