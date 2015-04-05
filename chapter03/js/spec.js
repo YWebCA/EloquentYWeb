827e91661bb68a66090613e1a41267df4f946123
Nathan Stier <nathan.stier@gmail.com>
2015-04-04 21:08:47 -0500

    Stub scope exercises
    Signed-off-by: Nathan Stier <nathan.stier@gmail.com>

--- chapter03/js/spec.js
+++ chapter03/js/spec.js
@@ -33,6 +33,56 @@ describe("Parameters and Scopes exercises", function() {
     it("should run the given spaceCubeDetector function with the arguments length => 7, width => 7, and height => 7 and log the return");
     it("should run the given spaceCubeDetector function with the arguments length => 2, width => 3, and height => 5 and log the return");
   });
+  /*
+  var foo = 2;
+  var bar = 7;
+  var quux = 11;
+
+  var skip = function() {
+    var foo = 5;
+    console.log("Scope 1: " + foo); // Scope 1: What value of foo is logged?
+    quux = foo *  bar;
+    console.log("Scope 2: " + quux); // Scope 2: What value of quux is logged?
+    return bar;
+  };
+
+  var scoop = function(quux) {
+    console.log("Scope 4: " + quux); // Scope 4: What value of quux is logged?
+  };
+
+  var scrap = function() {
+    console.log("Scope 5: " + quux); // Scope 5: What value of quux is logged?
+  };
+
+  foo = skip();
+  scoop(foo); // Scope 3: What value of foo is passed to the function Scoop?
+  scrap();
+  */
+  describe("Scope 1" function() {
+    it("should have the correct answer" function() {
+      expect( Exer.scope1() === 5).toBeTruthy();
+    });
+  });
+  describe("Scope 2" function() {
+    it("should have the correct answer" function() {
+      expect( Exer.scope2() === 35).toBeTruthy();
+    });
+  });
+  describe("Scope 3" function() {
+    it("should have the correct answer" function() {
+      expect( Exer.scope3() === 7).toBeTruthy();
+    });
+  });
+  describe("Scope 4" function() {
+    it("should have the correct answer" function() {
+      expect( Exer.scope4() === 35).toBeTruthy();
+    });
+  });
+  describe("Scope 5" function() {
+    it("should have the correct answer" function() {
+      expect( Exer.scope5() === 7).toBeTruthy();
+    });
+  });
 });
 
 /*****************************    Nested Scope    *****************************/
