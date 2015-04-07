006c2198e75fdce5387ac20bb058bbe250c7db1f
KoeSeph <koesavant@gmail.com>
2015-04-07 16:58:25 -0500

    js hint

--- chapter02/js/chessboard.js
+++ chapter02/js/chessboard.js
@@ -7,7 +7,7 @@ Chess.start = function (n) {
   var output = "";
   for ( var i = 0; i < size; i++ ) {
     for ( var j = 0; j < size; j++ ) {
-      if ( (i+j) % 2 == 0 ) {
+      if ( (i+j) % 2 === 0 ) {
         output += ' ';
       } else {
         output += '#';
