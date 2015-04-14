53ef189d7eb5f2059b700b4e9c5a38cff0320150
Annalise "Lulu" Haynes <lulu@lonelyrobotstudio.com>
2015-04-14 09:48:51 -0500

    Revert "Chapter two submit"

---
+++ js/enviroment.js
@@ -0,0 +1,28 @@
+// World enviroment file
+customMatchers = {};
+var jsHintOptions = {
+  curly: true,
+  white: true,
+  indent: 2,
+  maxdepth: 3,
+  bitwise: true,
+  eqeqeq: true,
+  nonew: true,
+  newcap: true,
+  maxstatements: 6,
+  notypeof: true,
+};
+
+// JSHint Matcher
+customMatchers.toPassJSHint =  function(util, customEqualityTesters) {
+  return {
+    compare: function(actual, expected) {
+      var pass = JSHINT(actual, jsHintOptions);
+      var message = "";
+      for (error of JSHINT.errors) {
+        message += "line " + error.line + " character " + error.character + ' - ' + error.reason + ' - ' + error.evidence + "\n";
+      }
+      return { pass: pass, message: message };
+    }
+  }
+}
