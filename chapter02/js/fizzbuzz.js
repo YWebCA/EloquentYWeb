f91615d3d031f73ea4d93776be529289b196670a
victoria conley <vconley4@icloud.com>
2015-04-21 17:43:51 -0500

    chapter02 games

--- chapter02/js/fizzbuzz.js
+++ chapter02/js/fizzbuzz.js
@@ -1,4 +1,17 @@
 // Student Workspace
 Fizz.buzz = function () {
-
+for (var i=1; i <= 100; i++)
+{
+    if (i % 15 == 0){
+        console.log("FizzBuzz");
+    }
+    else if (i % 3 == 0){
+        console.log("Fizz");
+    }    
+    else if (i % 5 == 0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);}
+}
 };
